397ae2065a59dd673f07fd95718765d9bae4091e
lee-tae-eon <mktears0609@gmail.com>
2021-09-08 12:02:56 +0900

    15.12 2:30

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -1,5 +1,110 @@
-import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { Button } from "../components/button";
+import { FormError } from "../components/form-error";
+import nuberLogo from "../images/logo.svg";
+import Helmet from "react-helmet";
+import {
+  loginMutation,
+  loginMutationVariables,
+} from "../__generated__/loginMutation";
+
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccountMutation($createAccountInput: CreateAccountInput!) {
+    createAccount(input: $createAccountInput) {
+      ok
+      error
+    }
+  }
+`;
+
+interface ILoginForm {
+  email: string;
+  password: string;
+}
 
 export const CreateAccount = () => {
-  return <span>Create</span>;
+  const {
+    register,
+    getValues,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
+
+  const [createAccountMutation] = useMutation(CREATE_ACCOUNT_MUTATION, {});
+
+  const onSubmit = () => {
+    if (!loading) {
+      const { email, password } = getValues();
+      loginMutation({
+        variables: {
+          loginInput: {
+            email,
+            password,
+          },
+        },
+      });
+    }
+  };
+
+  return (
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <Helmet>
+        <title>CreateAccount | Nuber-eats</title>
+      </Helmet>
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} alt="" className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-2xl">
+          Lets get started
+        </h4>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 mb-5 w-full"
+        >
+          <input
+            placeholder="Email"
+            className="login--input"
+            type="email"
+            {...register("email", { required: "Email is required" })}
+          />
+          {errors.password?.message && (
+            <FormError errorMessage={errors.email?.message} />
+          )}
+
+          <input
+            placeholder="Password"
+            className="login--input"
+            type="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: 2,
+            })}
+          />
+
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
+          {errors.password?.type === "minLength" && (
+            <span className="text-sm text-red-500">
+              비밀번호는 5자 이상이어야 합니다.
+            </span>
+          )}
+          <Button canClick={isValid} loading={loading} actionText={"Log in"} />
+
+          {loginMutationResult?.login.error && (
+            <FormError errorMessage={loginMutationResult.login.error} />
+          )}
+        </form>
+        <div>
+          New to Nuber ?{" "}
+          <Link to="/create-account" className="text-lime-600 hover:underline">
+            Create an Account
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,10 @@
 import { gql, useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { Button } from "../components/button";
 import { FormError } from "../components/form-error";
 import nuberLogo from "../images/logo.svg";
+import Helmet from "react-helmet";
 import {
   loginMutation,
   loginMutationVariables,
@@ -26,9 +29,11 @@ export const Login = () => {
   const {
     register,
     getValues,
-    formState: { errors },
+    formState: { errors, isValid },
     handleSubmit,
-  } = useForm<ILoginForm>();
+  } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
   const onCompleted = (data: loginMutation) => {
     const {
       login: { error, ok, token },
@@ -61,12 +66,15 @@ export const Login = () => {
 
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <Helmet>
+        <title>Login | Nuber-eats</title>
+      </Helmet>
       <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
         <img src={nuberLogo} alt="" className="w-52 mb-10" />
         <h4 className="w-full font-medium text-left text-2xl">Welcome back</h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 w-full"
+          className="grid gap-3 mt-5 mb-5 w-full"
         >
           <input
             placeholder="Email"
@@ -96,11 +104,18 @@ export const Login = () => {
               비밀번호는 5자 이상이어야 합니다.
             </span>
           )}
-          <button className="btn">{loading ? "Loading.." : "Log In"}</button>
+          <Button canClick={isValid} loading={loading} actionText={"Log in"} />
+
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error} />
           )}
         </form>
+        <div>
+          New to Nuber ?{" "}
+          <Link to="/create-account" className="text-lime-600 hover:underline">
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   );
