1bd1b6cea1da75b2ee911391057a6918585696fc
lee-tae-eon <mktears0609@gmail.com>
2021-10-27 22:18:20 +0900

    16.1 start

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,4 +1,6 @@
 import { gql, useMutation } from "@apollo/client";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
 import {
   verifyEmail,
   verifyEmailVariables,
@@ -18,4 +20,24 @@ export const ConfirmEmail = () => {
     verifyEmail,
     verifyEmailVariables
   >(VERIFY_EMAIL_MUTATION);
+
+  useEffect(() => {
+    const [_, code] = window.location.href.split("code=");
+    // verifyEmail({
+    //   variables: {
+    //     input: {
+    //       code,
+    //     },
+    //   },
+    // });
+  }, []);
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center mt-50">
+      <h2 className="text-lg mb-2 font-medium">Confirming email...</h2>
+      <h4 className="text-gray-700 text-sm">
+        Please wait, dont't close this page....
+      </h4>
+    </div>
+  );
 };
