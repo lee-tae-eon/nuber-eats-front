0b4b0039a6a0fdcfa907e74b5427ae80833d4253
lee-tae-eon <mktears0609@gmail.com>
2021-11-02 22:16:20 +0900

    졸림

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,13 +9,15 @@ const Header = () => {
   return (
     <>
       {!data?.me.verified && (
-        <div className="bg-red-500 py-3 px-3 text-white text-center text-xsm">
+        <div className="bg-red-500 py-3 px-3 text-white text-center text-base">
           <span>Please verify your email</span>
         </div>
       )}
       <header className="py-2.5">
         <div className="w-full px-3 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-          <img src={nuberLogo} alt="Nuber Eats" className="w-24" />
+          <Link to="/">
+            <img src={nuberLogo} alt="Nuber Eats" className=" w-32" />
+          </Link>
           <span className="text-xs">
             <Link to="/edit-profile">
               <FontAwesomeIcon icon={faUser} />
