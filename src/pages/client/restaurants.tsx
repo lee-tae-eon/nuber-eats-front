99d7ed9e04eebc55ee33933eb40c67479e5e4aa5
lee-tae-eon <mktears0609@gmail.com>
2021-11-05 05:55:19 +0900

    17.1 start

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
+import { url } from "inspector";
 import {
   restaurantsPageQuery,
   restaurantsPageQueryVariables,
@@ -53,9 +54,25 @@ const Restaurants = () => {
   }
   return (
     <div>
-      <div>
-        <input type="Search" placeholder="Search restaurants....." />
+      <div className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          className="input rounded-md border-0 w-4/12"
+          type="Search"
+          placeholder="Search restaurants....."
+        />
       </div>
+      {!loading && (
+        <div className="max-w-screen-xl mx-auto mt-8">
+          <div className="flex justify-around max-w-screen-xs mx-auto">
+            {data?.allCategories?.categories?.map((category) => (
+              <div
+                className=" w-12 h-12 rounded-full bg-red-500"
+                style={{ backgroundImage: `url(${category.coverImg})` }}
+              ></div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
